5a8f2b54b52be283a1b17c6ec763d875e6123d65
michelebri <83598165+michelebri@users.noreply.github.com>
2024-07-09 14:43:32 +0200

    add script to read image via socket

---
+++ python/receive_image.py
@@ -0,0 +1,49 @@
+import socket
+import numpy as np
+import cv2
+
+def start_server():
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server_socket.bind(('10.0.255.244', 5432))
+    
+    server_socket.listen(5)
+    print("Server in ascolto su 10.0.255.244:5432")
+    
+    while True:
+
+        client_socket, addr = server_socket.accept()
+        print(f"Connessione accettata da {addr}")
+        
+        try:
+
+            expected_size = 640 * 480
+            
+
+            image_data = b""
+            while len(image_data) < expected_size:
+                packet = client_socket.recv(expected_size - len(image_data))
+                if not packet:
+                    break
+                image_data += packet
+
+            if len(image_data) != expected_size:
+                print(f"Errore: dimensione dei dati ricevuti {len(image_data)} diversa dalla dimensione attesa {expected_size}")
+                continue
+
+
+            image_array = np.frombuffer(image_data, dtype=np.uint8).reshape((480, 640))
+            
+
+            cv2.imwrite("received_image.jpg", image_array)
+            
+            response = "Immagine ricevuta"
+            client_socket.send(response.encode('utf-8'))
+        except Exception as e:
+            print(f"Errore durante la comunicazione: {e}")
+        finally:
+            client_socket.close()
+
+if __name__ == "__main__":
+    start_server()
